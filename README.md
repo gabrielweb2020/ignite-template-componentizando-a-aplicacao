9a9e2f8f4b3b787fe59f6436120bc1d3b7f1bfba
Gabriel Bittencourt <gabriel.bc88@gmail.com>
2021-03-24 11:32:47 -0300

    initial project

---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center"> 
+  <a href="https://moveit-application.vercel.app/">
+    <img src="./public/screenshots/banner-ignite.png" />
+  </a>
+</p>
+<p align="center"> 
+  <h1>WatchMe</h1>
+</p>
+
+## How to install and use
+
+# Clone this repository
+$ git clone https://github.com/gabrielweb2020/ignite-template-componentizando-a-aplicacao
+
+# Access the project folder in the cmd/terminal
+$ cd ignite-template-componentizando-a-aplicacao
+
+# Install the dependencies using npm or yarn
+$ yarn install
+
+# Run the application in development mode
+$ yarn dev
+
+# The server will start at port:8080 - access  http://localhost:8080
+
+# Boot the server to use the application
+$ yarn server
+
+```
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  handleSelectedClickButton: (id: number) => void;
+  isSelectedGenreId: number;
+}
+
+export function SideBar({ handleSelectedClickButton, isSelectedGenreId }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleSelectedClickButton(genre.id)}
+            selected={isSelectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
+}
